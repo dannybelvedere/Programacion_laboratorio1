875647cb6a119adfe42ac582fbeca91319937f8d
DannyBelvedere <danny.salvatore@hotmail.com>
2018-04-04 18:58:44 -0300

    Clase 4- creacion de biblioteca util

---
+++ Clase_04/ejercicio/clase4/funciones.h
@@ -0,0 +1,21 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+int getInt(char* mensaje,
+           char mensajeError,
+           int reintentos,
+           int maximo,
+           int minimo,
+           int* resultado)
+{
+    int retorno = 1;
+
+    printf("%s", mensaje);
+
+    do
+    {
+
+    }while
+}
+
+
+#endif // FUNCIONES_H_INCLUDED
